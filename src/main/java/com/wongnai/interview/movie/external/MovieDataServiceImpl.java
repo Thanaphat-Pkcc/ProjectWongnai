29bc1f60281790f3e3bbd53056f1d67a8ff0fa8f
Thanaphat <yok_thanapat@hotmail.com>
2019-03-10 23:08:30 +0700

    MAPPING JSON TO OBJECT

--- src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
+++ src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
@@ -1,10 +1,29 @@
 package com.wongnai.interview.movie.external;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 @Component
 public class MovieDataServiceImpl implements MovieDataService {
@@ -16,6 +35,9 @@ public class MovieDataServiceImpl implements MovieDataService {
 
 	@Autowired
 	private ObjectMapper objectMapper;
+	
+	//MoviesResponse moviesResponse = new MoviesResponse();
+	//MovieData movieData = new MovieData();
 
 	@Override
 	public MoviesResponse fetchAll() {
@@ -23,7 +45,35 @@ public class MovieDataServiceImpl implements MovieDataService {
 		// Step 1 => Implement this method to download data from MOVIE_DATA_URL and fix any error you may found.
 		// Please noted that you must only read data remotely and only from given source,
 		// do not download and use local file or put the file anywhere else.
-
-		return null;
+		try {
+			String url = MOVIE_DATA_URL;
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+			
+			int responseCode = con.getResponseCode();
+			System.out.println("\nSending 'GET' request to URL : " + url);
+		     System.out.println("Response Code : " + responseCode);
+		     BufferedReader in = new BufferedReader(
+		             new InputStreamReader(con.getInputStream()));
+		     String inputLine;
+		     StringBuffer response = new StringBuffer();
+		     while ((inputLine = in.readLine()) != null) {
+		     	response.append(inputLine+"\n");
+		     	
+		     }
+		     in.close();
+		     
+		     Gson g = new Gson();
+		     
+		     //moviesResponse = new Gson().fromJson(response.toString(), new TypeToken<List<movieData>>(){}.getType());
+		     MoviesResponse moviesResponse = g.fromJson(response.toString(), MoviesResponse.class);
+		     return moviesResponse;
+		     //return null;
+		} catch (Exception e) {
+			// TODO: handle exception
+			return null;
+		}
+		
+		
 	}
 }
